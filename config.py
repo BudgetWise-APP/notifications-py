3fc36202d630c1edb78009457aac08d7b8a28541
voliinyk-blackbird <voliinyk@blackbird-lab.com>
2025-01-22 01:38:37 +0100

    config

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
-SMTP_HOST = os.getenv("SMTP_HOST")
-SMTP_PORT = os.getenv("SMTP_PORT")
+SMTP_HOST = "smtp.gmail.com"
+SMTP_PORT = 587
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 EMAIL_FROM = os.getenv("EMAIL_FROM", "budgetwise@example.com")
